a2d0bcd9666db40d7783041db6a58fc516686796
Sam <snippetsbysam@outlook.com>
2022-06-24 03:24:11 +0100

    add coffee model

---
+++ Models/Coffee.cs
@@ -0,0 +1,18 @@
+﻿namespace API_Interactive_Lab_1.Models
+{
+    public class Coffee
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public Coffee(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+        public override string ToString()
+        {
+            return $"Name:{Name} Id:{Id}";
+        }
+    }
+}
