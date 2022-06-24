73b2d792cb31d05949af75db352fd1a8a694bebb
Sam <snippetsbysam@outlook.com>
2022-06-24 02:01:04 +0100

    Add coffee lover Test

---
+++ Tests/Controllers/CoffeeControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using API_Interactive_Lab_1.Controllers;
+using NUnit.Framework;
+
+namespace API_Interactive_Lab_1.Tests.Controllers
+{
+    public class CoffeeControllerTests
+    {
+        [Test]
+        public void TestGetCoffeeLover()
+        {
+            string expectedContent = "I like coffee!";
+            var controller = new CoffeeController();
+            var result = controller.GetCoffeeLover();
+            Assert.AreEqual(expectedContent, result);
+        }
+    }
+}
