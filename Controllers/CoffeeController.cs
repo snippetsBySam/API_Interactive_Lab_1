22801b86de9ec737605008e7abaaf7b81299b922
Sam <snippetsbysam@outlook.com>
2022-06-24 02:04:36 +0100

    complete coffee lover

--- Controllers/CoffeeController.cs
+++ Controllers/CoffeeController.cs
@@ -1,6 +1,15 @@
-﻿namespace API_Interactive_Lab_1.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Interactive_Lab_1.Controllers
 {
-    public class CoffeeController
+    [ApiController]
+    [Route("[controller]")]
+    public class CoffeeController : ControllerBase
     {
+        [HttpGet("lover")]
+        public string GetCoffeeLover()
+        {
+            return "I like coffee!";
+        }
     }
 }
