26be3d2e193228fc44cd563bbe2f99aab8da269f
Sam <snippetsbysam@outlook.com>
2022-06-24 01:15:36 +0100

    Start coffee controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Interactive_Lab_1.Controllers
+{
+    [ApiController]
+    //[Route("")]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome to the Drinks API!";
+        }
+    }
+}
